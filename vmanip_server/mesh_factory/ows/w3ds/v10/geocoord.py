68275c41c0e1a478636e81ccfa5285bba40118d7
Anton L. Fuhrmann <fuhrmann@vrvis.at>
2014-03-05 17:35:15 +0100

    clean up

--- vmanip_server/mesh_factory/ows/w3ds/v10/geocoord.py
+++ vmanip_server/mesh_factory/ows/w3ds/v10/geocoord.py
@@ -5,15 +5,16 @@ coordrealEarthRadius = 6356752.3142
 coordradius = 1.0
 coordheightScale = coordradius / coordrealEarthRadius
 
-def fromGeoTo3D (geo):
-    dest=np.zeros((3))
+
+def fromGeoTo3D(geo):
+    dest = np.zeros(3)
     longInRad = math.radians(geo[0])
-    latInRad  = math.radians(geo[1])
-    cosLat = math.cos(latInRad);
-        
+    latInRad = math.radians(geo[1])
+    cosLat = math.cos(latInRad)
+
     # Take height into account
-    if (len(geo)>2):
-        radius= coordradius + coordheightScale * geo[2]
+    if len(geo) > 2:
+        radius = coordradius + coordheightScale * geo[2]
     else:
         radius = coordradius
 
@@ -21,5 +22,4 @@ def fromGeoTo3D (geo):
     dest[1] = radius * math.sin(longInRad) * cosLat
     dest[2] = radius * math.sin(latInRad)
 
-    return dest;
-    
+    return dest
