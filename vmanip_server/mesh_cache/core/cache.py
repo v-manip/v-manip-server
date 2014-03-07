a254fbc9c4635c78ab4645b3d4a71135eef6ac67
Martin Hecher <martin.hecher@fraunhofer.at>
2014-03-07 18:15:10 +0100

    Commit for testing communication between webclient-mesh_cache-mesh_factory

--- vmanip_server/mesh_cache/core/cache.py
+++ vmanip_server/mesh_cache/core/cache.py
@@ -30,6 +30,7 @@ from vmanip_server.mesh_cache import models
 from vmanip_server.mesh_cache import mapcache
 
 from eoxserver.core.decoders import kvp
+from eoxserver.services.result import ResultFile, result_set_from_raw_data
 from django.http import HttpResponse
 import json
 import urllib2
@@ -53,30 +54,9 @@ class MeshCache:
         tilerow_value = decoder.tilerow
         time_value = decoder.time
 
-        # 2. Store the retrieved textures in the MapCache:
-        mapcache_c = mapcache.Connection()
-        # FIXXME: get from content object!
-        try:
-            # filename = 'products/vrvis_demo/Reflectivity_2013137113720_0000.png'
-            filename = '/var/ngeob_autotest/data/test_data/test-tile.png'
-            texture = open(filename, 'r')
-        except:
-            print '[MeshCache] file: "' + filename + '" not readable'
-        # texture = open('/var/ngeob_autotest/data/reference_test_data/ASA_WS__0P_20100722_101601.jpg', 'r')
-        # print 'str size:   ' + str(len(texture.read()))
-        # texture.seek(0)
-        #print texture
-
-        # FIXXME: use decoder.time!
-        # time = '2012-05-17T10:50:00Z/2013-05-17T11:00:00Z'
-        time = ''
-        # FIXXME: use decoder.layer
-        layer = 'TEST_OSM'
-        mapcache_c.handle(layer, 'WGS84', time, decoder.tilecol, decoder.tilerow, decoder.tilelevel, texture)
-
         tilerow = None
 
-        # print 'Handling: ' + layer_name + '/' + str(tilelevel_value) + '/' + str(tilecol_value) + '/' + str(tilerow_value)
+        print 'Handling: ' + layer_name + '/' + str(tilelevel_value) + '/' + str(tilecol_value) + '/' + str(tilerow_value)
 
         layer_query = models.Layer.objects.filter(name=layer_name);
 
@@ -156,21 +136,19 @@ class MeshCache:
         # 1. Get the glTF content from the MeshFactory:
         content = self.get_tile_content_from_factory(decoder)
 
-        # # 2. Store the retrieved textures in the MapCache:
-        # mapcache_c = mapcache.Connection()
-        # # FIXXME: get from content object!
-        # # texture = open('products/vrvis_demo/Reflectivity_2013137113720_0000.png', 'r')
-        # texture = open('/var/ngeob_autotest/data/reference_test_data/ASA_WS__0P_20100722_101601.jpg', 'r')
-        # print texture
+        # 2. Store the retrieved textures in the MapCache:
+        mapcache_c = mapcache.Connection()
+        # FIXXME: get from content object!
+        try:
+            # filename = 'products/vrvis_demo/Reflectivity_2013137113720_0000.png'
+            filename = '/var/ngeob_autotest/data/test_data/test-tile.png'
+            texture = open(filename, 'r')
+        except:
+            print '[MeshCache] file: "' + filename + '" not readable'
 
-        # print 'vor texture.read()'
-        # blob = texture.read();
-        # print blob
-        # print 'nach texture.read()'
-
-        # # FIXXME: use decoder.time!
-        # time = '2013-05-01T13:00:00Z/2013-05-18T15:30:00Z'
-        # mapcache_c.handle(decoder.layer, 'WGS84', time, decoder.tilecol, decoder.tilerow, decoder.tilelevel, texture)
+        # FIXXME: use decoder.layer
+        layer = 'TEST_OSM'
+        mapcache_c.handle(layer, 'WGS84', decoder.time, decoder.tilecol, decoder.tilerow, decoder.tilelevel, texture)
 
         # 3. Rewrite .json to link to the WMTS url of the texture:
         # FIXXME!
@@ -190,13 +168,15 @@ class MeshCache:
 
         bbox_str = self.convert_tile_params_to_scene_bbox_string(tilelevel_value, tilecol_value, tilerow_value)
         
-        # FIXXME: after a refactoring of gettyle.py into at least a separated MeshCache class the baseurl should be a parameter of this class!
+        # FIXXME: after a refactoring of gettile.py into at least a separated MeshCache class the baseurl should be a parameter of this class!
         baseurl = 'http://localhost:8000/ows?service=W3DS&request=GetScene&version=1.0.0&crs=EPSG:4326&format=model/gltf'
-        url = '{0}&layer={1}&boundingBox={2}&time={3}'.format(baseurl, layer_value, bbox_str, time_value)
+        url = '{0}&layer={1}&bbox={2}&time={3}'.format(baseurl, layer_value, bbox_str, time_value)
 
         response = urllib2.urlopen(url)
         data = response.read()
         response.close()
+
+        print data
 
         return data
 
