correct normalizing of U coordinate

vmanip_server/mesh_factory/ows/w3ds/v10/getscene.py
@@ -190,7 +190,7 @@ class W3DSGetSceneHandler(Component):
                                 u_min = min (u_min, u)
                                 u_max = max (u_max, u)
 
-                    response.append("U: min=%f, max=%f<br>"%(u_min, u_max))
+                    print "U: min=%f, max=%f"%(u_min, u_max)
                     u_scale=u_max-u_min
                     # convert all clipped polylines to triangle strips
                     n=0
@@ -202,7 +202,7 @@ class W3DSGetSceneHandler(Component):
                                 for p in pl:
                                     x=p.x
                                     y=p.y
-                                    u = (p.u / u_scale + u_min) / (width + 1)  # normalize u to range [0,1]
+                                    u = (p.u / u_scale + u_min)  # normalize u to range [0,1]
                                     #print ("U(%5.2f %5.2f) X, Y=(%5.2f,%5.2f), " % (p.u, u, x, y))
                                     point = geocoord.fromGeoTo3D(np.array((x, y, heightLevelsList.min())))
                                     t.add_point(point, [u, 0], [0, 0, 1])